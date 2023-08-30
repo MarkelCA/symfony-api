3bcafee90a23e29f831d313d5b6d643f0d6a079c
MarkelCA <cuestaarribas.markel@proton.me>
2023-08-30 19:56:09 +0200

    Playing with routes

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    private array $users = [
+        '1' => [
+            'name' => 'markelca'
+        ]
+    ];
+
+    #[Route('/users', name: 'user_list')]
+    public function list(): Response
+    {
+        return $this->json($this->users);
+    }
+
+    #[Route('/users/{id}', name: 'user')]
+    public function user($id): Response
+    {
+        return $this->json($this->users[$id]);
+    }
+}
