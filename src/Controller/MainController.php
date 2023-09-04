9e1166dd6db8e992df8785ee402579aebf6d2a1d
MarkelCA <cuestaarribas.markel@proton.me>
2023-09-04 21:16:55 +0200

    Trying to use folders

---
+++ src/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main', name: 'main_route')]
+    public function list(): Response
+    {
+        return new Response("Main!");
+    }
+}
