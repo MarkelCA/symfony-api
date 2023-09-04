9c6f9b289f442cdc4f3852860cb96333817d5f22
MarkelCA <cuestaarribas.markel@proton.me>
2023-09-04 23:29:53 +0200

    Health check from ModuleA

--- src/ModuleA/Controller/HealthCheckController.php
+++ src/ModuleA/Controller/HealthCheckController.php
@@ -3,13 +3,9 @@ namespace ModuleA\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
-{
-    #[Route('/blog', name: 'blog_list')]
-    public function list(): Response
-    {
+class HealthCheckController extends AbstractController {
+    public function __invoke() : Response {
         return $this->json(['hello' => 'world']);
     }
 }
